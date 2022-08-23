60474082795b2cc0e6ca06a6085e68c704a3ec8b
truefibot <truebot.github@trusttoken.com>
2022-08-23 09:31:47 +0000

    🐕 Add `managerFeeBeneficiary` to `feeStrategy` (#3326)

--- interfaces/IFeeStrategy.sol
+++ interfaces/IFeeStrategy.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.10;
 
 interface IFeeStrategy {
+    function managerFeeBeneficiary() external view returns (address);
+
     function managerFee() external view returns (uint256);
 }
