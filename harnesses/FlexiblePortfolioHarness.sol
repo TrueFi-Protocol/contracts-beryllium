09debd8496de7678c1f1d0129b26e6921d48e80f
truefibot <truebot.github@trusttoken.com>
2022-09-21 08:42:50 +0000

    💵 Add repay spec to FlexiblePortfolio Beryllium (#3751)

--- harnesses/FlexiblePortfolioHarness.sol
+++ harnesses/FlexiblePortfolioHarness.sol
@@ -7,4 +7,36 @@ contract FlexiblePortfolioHarness is FlexiblePortfolio {
     function getBytes4(bytes calldata callData) public pure returns (bytes4) {
         return bytes4(callData);
     }
+
+    function getTotalAssetsAndFeeHarness()
+        public
+        view
+        returns (
+            uint256 totalAssetsAfterFee,
+            uint256 protocolFee,
+            uint256 managerFee
+        )
+    {
+        return getTotalAssetsAndFee();
+    }
+
+    function managerFeeBeneficiaryHarness() public view returns (address) {
+        return managerFeeBeneficiary();
+    }
+
+    function lastUpdateTimeHarness() public view returns (uint256) {
+        return lastUpdateTime;
+    }
+
+    function payManagerFeeHarness(
+        uint256 continuousFee,
+        uint256 actionFee,
+        uint256 liquidity
+    ) public returns (uint256) {
+        return payManagerFee(continuousFee, actionFee, liquidity);
+    }
+
+    function payProtocolFeeHarness(uint256 _fee, uint256 balance) public returns (uint256) {
+        return payProtocolFee(_fee, balance);
+    }
 }
