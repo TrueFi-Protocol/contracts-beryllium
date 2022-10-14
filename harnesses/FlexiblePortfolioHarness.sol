c2c62b4f5a76a2cb6d79cf5504206b02204fa166
truefibot <truebot.github@trusttoken.com>
2022-10-14 13:02:51 +0000

    🦁 Add tokenBalance and feePriority invariants to FP Be spec (#4153)

--- harnesses/FlexiblePortfolioHarness.sol
+++ harnesses/FlexiblePortfolioHarness.sol
@@ -2,10 +2,52 @@
 pragma solidity ^0.8.10;
 
 import "../FlexiblePortfolio.sol";
+import {IFixedInterestOnlyLoans} from "../interfaces/IFixedInterestOnlyLoans.sol";
 
 contract FlexiblePortfolioHarness is FlexiblePortfolio {
+    IFixedInterestOnlyLoans public fiol;
+
     function getBytes4(bytes calldata callData) public pure returns (bytes4) {
         return bytes4(callData);
+    }
+
+    function addInstrumentFIOL(
+        IDebtInstrument instrument,
+        IERC20Metadata _asset,
+        uint256 _principal,
+        uint16 _periodCount,
+        uint256 _periodPayment,
+        uint32 _periodDuration,
+        address _recipient,
+        uint32 _gracePeriod,
+        bool _canBeRepaidAfterDefault
+    ) external onlyRole(MANAGER_ROLE) returns (uint256) {
+        require(isInstrumentAllowed[instrument], "FP:Instrument not allowed");
+        uint256 instrumentId = fiol.issueLoan(
+            _asset,
+            _principal,
+            _periodCount,
+            _periodPayment,
+            _periodDuration,
+            _recipient,
+            _gracePeriod,
+            _canBeRepaidAfterDefault
+        );
+        require(instrument.asset(instrumentId) == asset, "FP:Token mismatch");
+        isInstrumentAdded[instrument][instrumentId] = true;
+        emit InstrumentAdded(instrument, instrumentId);
+
+        return instrumentId;
+    }
+
+    function updateInstrumentFIOL(
+        IDebtInstrument instrument,
+        uint256 instrumentId,
+        uint32 newGracePeriod
+    ) external onlyRole(MANAGER_ROLE) {
+        require(isInstrumentAllowed[instrument], "FP:Instrument not allowed");
+        fiol.updateInstrument(instrumentId, newGracePeriod);
+        emit InstrumentUpdated(instrument);
     }
 
     function getTotalAssetsAndFeeHarness()
