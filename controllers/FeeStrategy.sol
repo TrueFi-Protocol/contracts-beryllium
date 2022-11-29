35d7b300f7562552627947142628658725bc8c35
truefibot <truebot.github@trusttoken.com>
2022-11-29 04:02:36 +0000

    🤟🏽 Add AccessControl to FeeStrategy (#4731)

--- controllers/FeeStrategy.sol
+++ controllers/FeeStrategy.sol
@@ -3,20 +3,21 @@ pragma solidity ^0.8.10;
 
 import {IFeeStrategy} from "../interfaces/IFeeStrategy.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract FeeStrategy is IFeeStrategy, Initializable {
-    address public manager;
+contract FeeStrategy is IFeeStrategy, Initializable, AccessControlEnumerable {
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     uint256 public managerFeeRate;
 
     event ManagerFeeRateChanged(uint256 newManagerFeeRate);
 
-    function initialize(address _manager, uint256 _managerFeeRate) external initializer {
-        manager = _manager;
+    function initialize(address manager, uint256 _managerFeeRate) external initializer {
+        _setRoleAdmin(MANAGER_ROLE, MANAGER_ROLE);
+        _grantRole(MANAGER_ROLE, manager);
         managerFeeRate = _managerFeeRate;
     }
 
-    function setManagerFeeRate(uint256 newManagerFeeRate) external {
-        require(msg.sender == manager, "FeeStrategy: Only manager can set fee rate");
+    function setManagerFeeRate(uint256 newManagerFeeRate) external onlyRole(MANAGER_ROLE) {
         managerFeeRate = newManagerFeeRate;
         emit ManagerFeeRateChanged(newManagerFeeRate);
     }
