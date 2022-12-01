b36d4ced8689031095bd94fe7175d7c64c5626ab
truefibot <truebot.github@trusttoken.com>
2022-12-01 15:35:59 +0000

    🌨 Make feeStrategy immutable (#4772)

--- controllers/FeeStrategy.sol
+++ controllers/FeeStrategy.sol
@@ -5,20 +5,10 @@ import {IFeeStrategy} from "../interfaces/IFeeStrategy.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract FeeStrategy is IFeeStrategy, Initializable, AccessControlEnumerable {
-    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+contract FeeStrategy is IFeeStrategy, Initializable {
     uint256 public managerFeeRate;
 
-    event ManagerFeeRateChanged(uint256 newManagerFeeRate);
-
-    function initialize(address manager, uint256 _managerFeeRate) external initializer {
-        _setRoleAdmin(MANAGER_ROLE, MANAGER_ROLE);
-        _grantRole(MANAGER_ROLE, manager);
+    function initialize(uint256 _managerFeeRate) external initializer {
         managerFeeRate = _managerFeeRate;
-    }
-
-    function setManagerFeeRate(uint256 newManagerFeeRate) external onlyRole(MANAGER_ROLE) {
-        managerFeeRate = newManagerFeeRate;
-        emit ManagerFeeRateChanged(newManagerFeeRate);
     }
 }
