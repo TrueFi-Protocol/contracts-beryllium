64f78e332293a9cf9c1810f458520bb3ba201e77
truefibot <truebot.github@trusttoken.com>
2022-12-05 09:44:02 +0000

    🪟 Delete AccessControlEnumerable import from FeeStrategy (#4788)

--- controllers/FeeStrategy.sol
+++ controllers/FeeStrategy.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.10;
 
 import {IFeeStrategy} from "../interfaces/IFeeStrategy.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
 contract FeeStrategy is IFeeStrategy, Initializable {
     uint256 public managerFeeRate;
